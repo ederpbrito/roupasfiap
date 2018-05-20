61573edbb2fdbaa7d6f070ff76fde1ca39c30640
Eder Brito <eder.brito@resourceit.com>
2018-05-20 16:38:19 -0300

    commit initial

---
+++ src/main/java/br/com/loja/roupasfiap/RoupasfiapApplication.java
@@ -0,0 +1,12 @@
+package br.com.loja.roupasfiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoupasfiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoupasfiapApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/loja/roupasfiap/RoupasfiapApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.loja.roupasfiap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoupasfiapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
